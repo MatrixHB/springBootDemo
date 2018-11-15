cbadad022c9bafb207caba730b8a773b6f5d518d
xu_yibin <https://github.com/MatrixHB/springDemo.git>
2018-11-16 00:28:40 +0800

    Restful CRUD completed

--- src/main/java/controller/BusdataController.java
+++ src/main/java/controller/BusdataController.java
@@ -5,10 +5,11 @@ import entities.BusData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 public class BusdataController {
@@ -16,13 +17,55 @@ public class BusdataController {
     @Autowired
     BusDataDao busDataDao;
 
+    //查询节点信息
     @GetMapping("/busdata")
     public String list(Model model) throws Exception{
 
-        List<BusData> list = busDataDao.queryBusData();
+        Map<Integer, BusData> map = busDataDao.getMap();
+        Collection<BusData> list = map.values();
         model.addAttribute("busdata",list);
         //thymrleaf默认拼串“classpath:/busdata/list.html”
         return "busdata/list";
     }
+
+    //来到节点添加页面
+    @GetMapping("/addBus")
+    public String toAddPage(){
+        return "busdata/add";
+    }
+
+    //添加节点信息完成并回到列表页面
+    //springMVC自动将请求参数和入参对象的属性进行一一绑定，要求请求参数的名字和入参对象javaBean中属性名一致
+    @PostMapping("/bus")
+    public String addBus(BusData busData){
+        busDataDao.saveBusData(busData);
+        //表示重定向到“查询节点信息”的请求
+        return "redirect:/busdata";
+    }
+
+    //来到节点修改页面（与添加页面二合一），busNumber是请求路径上的变量
+    @GetMapping("/busdata/{busNumber}")
+    public String toEditPage(@PathVariable Integer busNumber, Model model){
+
+        //需要回显待编辑节点的信息，所以要传回一个与busNumber对应的model
+        BusData busData = busDataDao.getMap().get(busNumber);
+        model.addAttribute("bus",busData);
+
+        return "busdata/add";
+    }
+
+    //修改节点信息完成并回到列表页面
+    @PutMapping("/bus")
+    public String editBus(BusData busData){
+        busDataDao.saveBusData(busData);
+        return "redirect:/busdata";
+    }
+
+    @DeleteMapping("/busdata/{busNumber}")
+    public String deleteBus(@PathVariable Integer busNumber){
+        busDataDao.deleteBusData(busNumber);
+        return "redirect:/busdata";
+    }
+
 }
 
--- src/main/java/dao/BusDataDao.java
+++ src/main/java/dao/BusDataDao.java
@@ -9,17 +9,28 @@ import org.springframework.stereotype.Repository;
 
 import javax.security.auth.login.Configuration;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class BusDataDao {
+
+    private static Map<Integer, BusData> map = new HashMap<>(600);
 
     private static String driver = "oracle.jdbc.driver.OracleDriver";
     private static String url = "jdbc:oracle:thin:@10.15.194.25:1521:idpora";
     private static String username = "RDS-GY";
     private static String password = "123456";
 
-    public List<BusData> queryBusData() throws Exception {
-        List<BusData> list = new ArrayList<BusData>(600);
+    {
+        try {
+            map = queryBusData();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public Map<Integer, BusData> queryBusData() throws Exception{
 
         Class.forName(driver);
         Connection conn = DriverManager.getConnection(url, username, password);
@@ -29,16 +40,34 @@ public class BusDataDao {
 
         while (res.next()) {
             BusData busData = new BusData();
-            busData.setBusNumber(res.getInt("busnumber"));
+            int busNumber = res.getInt("busnumber");
+            if(map.keySet().contains(busNumber))
+                continue;
+            busData.setBusNumber(busNumber);
             busData.setBusName(res.getString("busname"));
             busData.setBusLoad(res.getDouble("busload"));
             busData.setDeviceName(res.getString("devicename"));
-            list.add(busData);
+            map.put(busData.getBusNumber(),busData);
         }
         res.close();
         stmt.close();
         conn.close();
 
-        return list;
+        return map;
     }
+
+    public void saveBusData(BusData busData){
+
+        map.put(busData.getBusNumber(), busData);
+    }
+
+    public void deleteBusData(Integer busNumber){
+
+        map.remove(busNumber);
+    }
+
+    public static Map<Integer, BusData> getMap() {
+        return map;
+    }
+
 }
